eb4ed1ea021dc5d4b76e3c4558666fffb5e92ef0
joshdev <joshuasol4950@gmail.com>
2023-03-29 11:35:33 +0800

    modified some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+    },
+    container: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+      },
+      center: true,
+    },
+    fontFamily: {
+      body: ["Montserrat", "sans-serif", "system-ui", "-apple-system"],
+    },
     extend: {},
   },
   plugins: [],
